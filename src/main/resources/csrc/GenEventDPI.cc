7e0f7fecc05fb38f8649cbf79fb288c76810e7c3
Kevin He <kevinjhe@berkeley.edu>
2024-11-15 01:56:43 -0800

    dpi-c outputs logs to file

--- src/main/resources/csrc/GenEventDPI.cc
+++ src/main/resources/csrc/GenEventDPI.cc
@@ -2,18 +2,26 @@
 #include <vector>
 #include <fstream> 
 #include <iostream> 
+#include <string>
+#include <zlib.h>
 
 class EventLog {
-public:
-    void add_event(char* event_name,
+public:   
+    void add_event(std::string event_name,
                     long long int id,
                     long long int parent,
                     long long int cycle,
                     long long int data) {
-        event_entry new_event = {event_name, id, parent, cycle, data};
+        event_entry new_event{event_name, id, parent, cycle, data};
+        // std::cout << event_name << "\n";
+        // std::cout << "hi there!" << "\n";
         event_vector.push_back(new_event);
     }
+    EventLog() { 
+        std::cout << "EventLog constructor called\n";
+    }
     ~EventLog() {
+        std::cout << "EventLog destructor called\n";
         std::ofstream file("GenEventLog.txt");
         if (file.is_open()) {
             for (const auto& event : event_vector) {
@@ -26,14 +34,14 @@ public:
                 ;
             }
             file.close();
-            std::cout << "Events written to file.\n";
+            std::cout << "Events successfully written to file.\n";
         } else {
             std::cerr << "Event log FAILED.\n";
         }
     }
 private:
     struct event_entry {
-        char * event_name;
+        std::string event_name;
         int64_t id;
         int64_t parent;
         int64_t cycle;
